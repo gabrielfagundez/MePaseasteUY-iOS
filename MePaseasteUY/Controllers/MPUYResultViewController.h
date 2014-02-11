6cd6b80114556dcf1fd3a4ed49ff48077bab30fe
Gabriel Fagundez <gabriel.fagundez@moove-it.com>
2014-02-11 19:28:40 -0200

    Nueva vista y controladora asociada

---
+++ MePaseasteUY/Controllers/MPUYResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPUYResultViewController.h
+//  MePaseasteUY
+//
+//  Created by Gabriel Fagundez de los Reyes on 11/02/14.
+//  Copyright (c) 2014 Gabriel Fagundez de los Reyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPUYResultViewController : UIViewController
+
+@end
---
+++ MePaseasteUY/Controllers/MPUYResultViewController.m
@@ -0,0 +1,38 @@
+//
+//  MPUYResultViewController.m
+//  MePaseasteUY
+//
+//  Created by Gabriel Fagundez de los Reyes on 11/02/14.
+//  Copyright (c) 2014 Gabriel Fagundez de los Reyes. All rights reserved.
+//
+
+#import "MPUYResultViewController.h"
+
+@interface MPUYResultViewController ()
+
+@end
+
+@implementation MPUYResultViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
